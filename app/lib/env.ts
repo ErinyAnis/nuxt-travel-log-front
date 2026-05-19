efd404eeccd5ee4b3ef594f357eb26bfdb2c18cb
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2026-05-19 12:52:57 +0300

    set up database

--- app/lib/env.ts
+++ app/lib/env.ts
@@ -1,7 +1,9 @@
 import { z } from 'zod';
 
 const envSchema = z.object({
-  NODE_ENV: z.enum(['development', 'production', 'test']),
+    NODE_ENV: z.enum(['development', 'production', 'test']),
+    TURSO_DATABASE_URL: z.string(),
+    TURSO_AUTH_TOKEN: z.string(),
 });
 
 export type Env = z.infer<typeof envSchema>;
