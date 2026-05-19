fa4f79ba48ffc7dc401ff2b0ca47c347c202309f
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2026-05-19 15:20:48 +0300

    fix build

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,22 +1,16 @@
-import tailwindcss from "@tailwindcss/vite";
-// import "./app/lib/env";
-
-// https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2025-07-15',
+
   devtools: { enabled: true },
-  modules: ['@nuxt/eslint', '@nuxt/icon', '@nuxtjs/color-mode'],
-  // eslint: {
-  //   config: {
-  //     standalone: false
-  //   }
-  // },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxtjs/color-mode'
+  ],
+
   css: ['~/assets/css/main.css'],
-  vite: {
-    plugins: [
-      tailwindcss(),
-    ],
-  },
+
   colorMode: {
     dataValue: 'theme',
   }
