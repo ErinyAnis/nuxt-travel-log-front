3e3cf866ee90ed9f3083178022e20251f3781b23
Eriny Anis <124495990+ErinyAnis@users.noreply.github.com>
2026-05-20 17:46:29 +0300

    create new locations

--- server/middleware/auth.ts
+++ server/middleware/auth.ts
@@ -1,11 +1,13 @@
 import { auth } from "~/lib/auth";
 
 export default defineEventHandler(async (event) => {
-    if(event.path.startsWith("/dashboard")) {
-        const session = await auth.api.getSession({
+    const session = await auth.api.getSession({
             headers: event.headers,
         });
-        if(!session?.user) {
+        event.context.user= session?.user;
+    if (event.path.startsWith("/dashboard")) {
+
+        if (!session?.user) {
             await sendRedirect(event, '/', 302)
         }
     }
